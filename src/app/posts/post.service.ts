f7720ee5d116e3de219fd80c68eec518a36f191b
sameer <sameerbhati146@gmail.com>
2020-01-31 02:19:10 -0800

    Adding node server and express

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,24 +1,37 @@
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostService {
-  private  posts: Post[] = [];
+  private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        console.log(postData, 'response');
+        this.posts = postData.posts;
+        console.log(this.posts, 'posts from server');
+        this.postUpdated.next([...this.posts]);
+      });
   }
 
-  getPostUpdatedListner(){
+  getPostUpdatedListner() {
     return this.postUpdated.asObservable();
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title, content};
-    this.posts.push(post);
-    this.postUpdated.next([...this.posts]);
+    const post: Post = {id: null, title, content};
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseDate) => {
+        console.log(responseDate.message);
+        this.posts.push(post);
+        this.postUpdated.next([...this.posts]);
+      });
   }
 }
